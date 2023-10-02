459c6f2e6e5ea1324d4b4f7308e055772f2ea46d
Sai Varshini <saivarshini.chinnasani@gmail.com>
2023-10-01 21:42:30 -0500

    MeepMeep Test
    Signed-off-by: Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>

--- MeepMeep/src/main/java/com/example/meepmeep/BlueBackStage.java
+++ MeepMeep/src/main/java/com/example/meepmeep/BlueBackStage.java
@@ -13,15 +13,13 @@ public class BlueBackStage {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(15, 46, 270))
+                                .back(10)
+                                .turn(0.5*Math.PI)
+                                .lineToSplineHeading(new Pose2d(56, 46))
+
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
                                 .build()
                 );
 
