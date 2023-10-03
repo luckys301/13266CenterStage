b04ffe8f8732712286a945be20b4cf5c5d128d4e
Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
2023-10-03 00:00:11 -0500

    Messed with MeepMeep
    Signed-off-by: Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>

--- MeepMeep/src/main/java/com/example/meepmeep/RedWing.java
+++ MeepMeep/src/main/java/com/example/meepmeep/RedWing.java
@@ -1,6 +1,7 @@
 package com.example.meepmeep;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,14 +14,12 @@ public class RedWing {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(-34,-52, 90))
+                                .splineTo(new Vector2d(-34, -34), Math.toRadians(90))
+                                .splineTo(new Vector2d(-60, -34), Math.toRadians(180))
+                                .back(50)
+                                .forward(50)
+                                .back(50)
                                 .turn(Math.toRadians(90))
                                 .build()
                 );
