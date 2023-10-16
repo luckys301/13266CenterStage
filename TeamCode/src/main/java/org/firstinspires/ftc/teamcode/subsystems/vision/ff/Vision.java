3475b453cefcfc4f14984cfc9fd7b82270d3e88e
Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
2023-10-16 16:50:08 -0500

    Tested Out Positions on Camera
    Signed-off-by: Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/ff/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/ff/Vision.java
@@ -4,36 +4,33 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.util.misc.Util;
-
-import java.util.logging.Level;
 
 
 public class Vision extends SubsystemBase {
-    private Telemetry telemetry;
+    private final Telemetry telemetry;
     private final FFRectDetector duckDetector;
-    private TeamMarkerPipeline.Position currentPos;
+//    private TeamMarkerPipeline.Position currentPos;
 
     public Vision(HardwareMap hw, Telemetry tl) {
         duckDetector = new FFRectDetector(hw, tl);
         duckDetector.init();
 
-        duckDetector.setLeftRectangle(0.12, 0.25);
-        duckDetector.setCenterRectangle(0.51, .25);
-        duckDetector.setRightRectangle(0.90, .25);
-        duckDetector.setRectangleSize(1,1);
+        duckDetector.setLeftRectangle(.4, .5);
+        duckDetector.setCenterRectangle(.3, .2);
+        duckDetector.setRightRectangle(.8, .5);
+        duckDetector.setRectangleSize(50,50);
         telemetry = tl;
-        currentPos = duckDetector.getPosition();
+//        currentPos = duckDetector.getPosition();
     }
 
 
     @Override
     public void periodic() {
-        currentPos = duckDetector.getPosition();
-        Util.logger(this, telemetry, Level.INFO, "Duck Position: ", duckDetector.getPosition());
+//        currentPos = duckDetector.getPosition();
+        telemetry.addData("Position", duckDetector.getPosition());
     }
 
-    public TeamMarkerPipeline.Position getCurrentPosition() {
-        return currentPos;
+    public TeamMarkerPipeline.FFPosition getPosition() {
+        return duckDetector.getPosition();
     }
 }
